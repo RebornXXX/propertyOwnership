597d9c52ca90a6864a0f689a54cc180cf014517a
RebornXXX <shivamambadkar@outlook.com>
2023-11-04 19:43:18 +0530

    created Land class

---
+++ propertyOwnership/lib/src/main/java/propertyOwnership/Land.java
@@ -0,0 +1,73 @@
+package propertyOwnership;
+
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+import java.util.Objects;
+ 
+@DataType()
+public final class Land {
+ 
+	@Property()
+	private final String id;
+ 
+	@Property()
+	private final String location;
+ 
+	@Property()
+	private final String owner;
+ 
+	@Property()
+	private final String value;
+ 
+	public String getId() {
+		return id;
+	}
+ 
+	public String getLocation() {
+		return location;
+	}
+	public String getOwner() {
+		return owner;
+	}
+ 
+	public String getValue() {
+		return value;
+	}
+ 
+	public Land(@JsonProperty("id") final String id, @JsonProperty("model") final String model, @JsonProperty("owner") final String owner,
+			@JsonProperty("value") final String value) {
+		this.id = id;
+		this.location = model;
+		this.owner = owner;
+		this.value = value;
+	}
+ 
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+ 
+		if ((obj == null) || (getClass() != obj.getClass())) {
+			return false;
+		}
+ 
+		Land other = (Land) obj;
+ 
+		return Objects.deepEquals(new String[] { getId(), getLocation(), getOwner(), getValue() },
+				new String[] { other.getId(), other.getLocation(), other.getOwner(), other.getValue() });
+	}
+ 
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId(), getLocation(), getOwner(), getValue());
+	}
+ 
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + "@" + Integer.toHexString(hashCode()) + " [id=" + id + ", model=" + location
+				+ ", owner=" + owner + ", value=" + value + "]";
+	}
+ 
+}
